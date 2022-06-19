fd4b3970061bed3821d0d8a518da94a62342f933
Sam Miller <sammybobmiller1@gmail.com>
2022-06-19 20:18:26 +0100

    Initial Commit

---
+++ Yorkshire 2UITests/Yorkshire_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Yorkshire_2UITestsLaunchTests.swift
+//  Yorkshire 2UITests
+//
+//  Created by Samuel Miller on 19/06/2022.
+//
+
+import XCTest
+
+class Yorkshire_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
